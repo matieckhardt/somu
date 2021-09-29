6622d88375287f4bc53fdd1feae7c24d8b91979e
matieckhardt <matias.eckhardt@gmail.com>
2021-09-29 18:12:57 -0500

    chau htmls,  terminado vista de prodetails

--- routes/routes.js
+++ routes/routes.js
@@ -17,7 +17,7 @@ router.get("/", async (req, res)  => {
       slideUrl: HomeData,
     })
   ); 
-  res.render("index", {Slides: datos});
+  res.render("index", {Slides: datos, headContent:'Home' });
 } catch (error) {
   console.log("ups", error);
 }
@@ -34,35 +34,71 @@ router.get("/Categories", async (req, res)  => {
       nombre: CategoriaName,     
     })
   ); 
-  res.render("categories", {categorias: datos});
+  res.render("categories", {categorias: datos, headContent:'Categorias' });
 } catch (error) {
   console.log("ups", error);
 }
 });
 
-router.get("/Productos/:subcat/:detalles", function (req, res) {
-  res.render("/prodetails");
-});
+router.get("/Productos/Detalles/:name",  async (req, res) => {
+  try {
+    const Detalles = await Productos.find();
+    const fotos = Detalles[0].imgCar.map(
+      (imgCar) => ({ fotos: imgCar})
+    );
+    console.log(fotos)
+    const datos = Detalles.map(
+      ({ Title, 
+        MainImageURL, 
+        Description, 
+        Resumen,
+        SpecA,
+        SpecB,
+        SpecC,
+        SpecD,
+        SpecE,
+        SubCategoriaName,
+        LinkUrl,  }) => ({
+        Titulo: Title,
+        img: MainImageURL,
+        Descrip: Description,
+        Resumen: Resumen,
+        SpA: SpecA,
+        SpB: SpecB,
+        SpC: SpecC,
+        SpD: SpecD,
+        SpE: SpecE,
+        link: LinkUrl,
+        SubCat: SubCategoriaName,
+    
+      })
+    ); 
+    res.render("prodetails", {Car: fotos, Detalles: datos,   headContent:'Detalles' });
+  } catch (error) {
+    console.log("ups", error);
+  }
+  });
+
 router.get("/AboutUs", function (req, res) {
-  res.render("about-us");
+  res.render("about-us", { headContent:'Nosotros' } );
 });
 router.get("/Faq", function (req, res) {
-  res.render("faq");
+  res.render("faq", { headContent:'FAQ' });
 });
 router.get("/Empresas", function (req, res) {
-  res.render("empresas");
+  res.render("empresas", { headContent:'Empresas' });
 });
 router.get("/Suv", function (req, res) {
-  res.render("suv");
+  res.render("suv", { headContent:'Soluciones UV' });
 });
 router.get("/Contact", function (req, res) {
-  res.render("contact");
+  res.render("contact", { headContent:'Contacto' });
 });
 router.get("/Form", function (req, res) {
-  res.render("form");
+  res.render("form", { headContent:'Form' });
 });
 router.get("/Shop", function (req, res) {
-  res.render("shop");
+  res.render("shop", { headContent:'Shop' });
 });
 
 // Rutas de las Sub Categorias
@@ -78,6 +114,7 @@ router.get("/Categorias/:subcat", async (req, res) => {
     res.render("subcat", {
       SubCategorias: datos,
       CategoriaName: req.params.subcat,
+      headContent:'SubCategorias',
     });
   } catch (error) {
     console.log("ups", error);
@@ -100,6 +137,7 @@ router.get("/Categorias/:subcat/:producto", async (req, res) => {
     res.render("products", {
       Productos: datos,
       ProductoName: req.params.producto,
+      headContent:'Productos',
     });
   } catch (error) {
     console.log("ups", error);
@@ -112,21 +150,18 @@ router.get("/Categorias/:subcat/:producto", async (req, res) => {
 router.post("/postCar", async (req, res) => {
   const datos = [
     {
-      ProyectId: 1,
       MainImageURL: "/img/projects/romanos.jpg",
-      Title: "Crono",
-      Description: "Crono",
-      ShortDescription: "",
-      State: "",
-      Address: "",
-      Year: 0,
-      Size: 0,
-      Rooms: 0,
-      Visible: 1,
-      CategoriaId: 1,
-      CreatedBy: 1,
-      TourURL: "",
+      Title: "Romanos",
+      Description: " la Descripcion de este baño romano",
+      Resumen: "Es el resumen del producto baños romanos",
+      SpecA: "400m",
+      SpecB: "50kg",
+      SpecC: "electrico",
+      SpecD: "motor de agua",
+      SpecE: "otra especificacion",
+      LinkUrl: "https://www.youtube.com/watch?v=nOhse9-3-O0&ab_channel=ARTEHISTORIA",
       SubCategoriaName: "Romanos",
+      imgCar: ["/img/productos/romanos1.jpg","/img/productos/romanos2.jpg", "/img/productos/romanos3.jpg"]
     },
   ];
   try {
